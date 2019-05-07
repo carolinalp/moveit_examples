394fdb4fa297b06f11e09d99750b579fedefaa1b
Carolina Leon P <carolinalp@gmail.com>
2019-05-07 15:45:17 +0000

    Comander ready but pick and place still with errors

--- exampleUR10/src/caro_moveit_commander.py
+++ exampleUR10/src/caro_moveit_commander.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-import sys
 import copy
-import rospy
+import sys
+from math import pi
+
+import geometry_msgs.msg
 import moveit_commander
 import moveit_msgs.msg
-import geometry_msgs.msg
-from math import pi
-from std_msgs.msg import String
+import rospy
 from moveit_commander.conversions import pose_to_list
+from std_msgs.msg import String
+import time
 
+#=============================          CLASS          =================================
 
 class MoveCommanderUr10 (object): 
     """ Move commander created by CLP with the template of MoveGroupPythonInterfaceTutorial"""
@@ -33,8 +36,11 @@ class MoveCommanderUr10 (object):
         self.planning_frame = self.group.get_planning_frame()
         self.eef_link= self.group.get_end_effector_link()
         self.group_names = self.robot_comm.get_group_names()
+        self.box_name= ""
+        self.box_pose = geometry_msgs.msg.PoseStamped()    
 
-   
+#===========================      PLAN       =========================================
+
     def plan_to_pose_target (self, position_x, position_y, position_z, orient_x, orient_y, orient_z, orient_w):
 
         pose_goal = geometry_msgs.msg.Pose()
@@ -53,64 +59,119 @@ class MoveCommanderUr10 (object):
 
         return plan
 
+    
+    def go_to_joint_target (self,shoulder_pan, shoulder_lift, elbow, wrist_1, wrist_2, wrist_3):
+        """shoulder_pan, shoulder_lift, elbow, wrist_1, wrist_2, wrist_3 are the position 
+        values of these joints"""
+
+        joint_goal = self.group.get_current_joint_values()
+        joint_goal[0] = shoulder_pan
+        joint_goal[1] = shoulder_lift
+        joint_goal[2] = elbow
+        joint_goal[3] = wrist_1
+        joint_goal[4] = wrist_2
+        joint_goal[5] = wrist_3
+
+        plan = self.group.plan(joint_goal)
+        return plan
+
+
     def execute_plan (self, plan):
         
         self.group.execute(plan, wait=True)
 
 
-    # def go_to_joint_target (self, )
+    # def go_to_pregrasp_position (self, object_pose, pregrasp_distance=0.3):
+    #     """ It should be already a box added to the scene"""
+            
+    #     if type (object_pose) is not geometry_msgs.msg.PoseStamped(): 
+    #         rospy.logerr ( "The object pose should be a geometry_msgs.msg.PoseStamped()")
+    #         return 
+
+    #     else:  
+    #         wpose = object_pose
+    #         wpose.position_z += 0.3 #Go to 30 cm above the box 
+    #         plan_to_pose_target (self,  object_pose.position.x, object_pose.position.y, object_pose.position.z, object_pose.orientation.x, object_pose.orientation.y, object_pose.orientation.z, object_pose.orientation.w)    
 
 
+#============================       BOX     =============================================
+
+    def add_box(self, position_x, position_y, position_z, timeout=4):
+        
+        """Adding Objects to the Planning Scene
+        position_x, position_y, position_z are floats. The position in the 
+        planning scene where you want the box"""
+        
+        self.box_name = "box"
+
+            
+        self.box_pose.header.frame_id = "world"
+        self.box_pose.pose.position.x = position_x
+        self.box_pose.pose.position.y = position_y
+        self.box_pose.pose.position.z = position_z
+        self.box_pose.pose.orientation.w = 1.0
+        
+        self.scene_interface.add_box(self.box_name, self.box_pose, size= (0.1, 0.1, 0.1)) 
+
+    def attach_box(self, timeout=4):
+        touch_links = ["hand_base_attach"]
+        self.scene_interface.attach_box(self.eef_link, self.box_name, touch_links=touch_links)
+
+    def detach_box(self, timeout= 4):
+        
+        self.scene_interface.remove_attached_object(self.eef_link, name=self.box_name)
+    
+    def remove_box(self, timeout=4):
+        
+        self.scene_interface.remove_world_object(self.box_name)
+#===============================================================================================================
+#                                              Main   
+#=============================================================================================================
+        
 if __name__ == '__main__':
     
     rospy.init_node("move_commander_ur10", anonymous= True) # Define the node
 
-    ur10_commander= MoveCommanderUr10()
+    ur10_commander= MoveCommanderUr10()    
+#     #Random pose
     
-    #Random pose
-    plan1= ur10_commander.plan_to_pose_target(0.89,0.36,0.5,0,0.707,0.707 ,0)
-     
-    if len(plan1.joint_trajectory.points)>0: 
-        
-        raw_input("if Plan is ok. press enter to execute")
-    else: 
-        rospy.logerr ( "Fail to make a plan")
-
-    ur10_commander.execute_plan(plan1)
    
          
     # Pose to vertical axis (i)
-    plan_vertical= ur10_commander.plan_to_pose_target(0.04016, 0.36714, 1.52268,0.00021975, 0.70679, 0.70742, -0.00028481)
-     
-    if len(plan_vertical.joint_trajectory.points)>0: 
-        
-        raw_input("if Plan is ok. press enter to execute")
-  
-    else: 
-        rospy.logerr ( "Fail to make a plan")
-
-    ur10_commander.execute_plan(plan_vertical)
+    
 
      # Pose to horizontal axis (1)
-    plan_horizm1= ur10_commander.plan_to_pose_target(-1.2768, 0.36727, 0.26319,-0.013261,0.70689,0.70708 ,0.012899)
+   
+
+    # # Pose to horizontal axis (1)
+    # plan_horiz1= ur10_commander.plan_to_pose_target(1.2768, 0.36727, 0.26319,-0.0092237,0.707,0.7071 ,0.0084781)
      
-    if len(plan_horizm1.joint_trajectory.points)>0: 
-        
+    # if len(plan_horiz1.joint_trajectory.points)>0: 
+    #     print "Plan to -1 "
+    #     raw_input("if Plan is ok. press enter to execute")
+  
+    # else: 
+    #     rospy.logerr ( "Fail to make a plan")
+
+    # ur10_commander.execute_plan(plan_horiz1)
+    
+
+    #Go to a defined joint state
+
+    plan_with_joints = ur10_commander.go_to_joint_target(-0.03883398363815905, -1.5276687999544085, 1.5388371215293597, 3.1372948604019957, 0.039511801410542426, 3.134863975410523)
+    
+    if len(plan_with_joints.joint_trajectory.points)>0: 
+        print "Plan to joint state "
         raw_input("if Plan is ok. press enter to execute")
   
     else: 
         rospy.logerr ( "Fail to make a plan")
 
-    ur10_commander.execute_plan(plan_horizm1)
+    ur10_commander.execute_plan(plan_with_joints)
 
-    # Pose to horizontal axis (1)
-    plan_horiz1= ur10_commander.plan_to_pose_target(1.2768, 0.36727, 0.26319,-0.0092237,0.707,0.7071 ,0.0084781)
-     
-    if len(plan_horiz1.joint_trajectory.points)>0: 
-        
-        raw_input("if Plan is ok. press enter to execute")
-  
-    else: 
-        rospy.logerr ( "Fail to make a plan")
+    #Add a box
+      
 
-    ur10_commander.execute_plan(plan_horiz1)
+   
+    
+    
---
+++ exampleUR10/src/caro_pick_and_place.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+import copy
+import sys
+from math import pi
+
+import geometry_msgs.msg
+import moveit_commander
+import moveit_msgs.msg
+import rospy
+from moveit_commander.conversions import pose_to_list
+from std_msgs.msg import String
+import time
+
+from caro_moveit_commander import MoveCommanderUr10
+
+class PickAndPlace (object):
+    
+    """ This class moves the robot to initial positions, add a box in position1
+    grasp the box and move it to the position2, then deatach the box and remove 
+    it. Assumes that position1 and position2 are elements of the type 
+    geometry_msgs.msg.PoseStamped """
+    
+
+    def __init__(self, position1, position2):
+        
+        self.ur10_commander= MoveCommanderUr10()
+        self.position1= position1
+        self.position2 = position2
+        self.go_to_home_position()           
+        self.ur10_commander.add_box(self.position1.pose.position.x, self.position1.pose.position.y, self.position1.pose.position.z )
+        time.sleep (5)
+        self.box_pose=self.ur10_commander.box_pose
+        self.ur10_commander.remove_box()
+        self.go_to_pregrasp_position(self.box_pose)
+
+
+    def go_to_home_position(self): 
+
+        plan_with_joints = self.ur10_commander.go_to_joint_target(-0.03883398363815905, -1.5276687999544085, 1.5388371215293597, 3.1372948604019957, 0.039511801410542426, 3.134863975410523)
+    
+        if len(plan_with_joints.joint_trajectory.points)>0: 
+            print "Plan to joint state "
+            raw_input("if Plan is ok. press enter to execute")
+
+        else: 
+            rospy.logerr ( "Fail to make a plan")
+
+        self.ur10_commander.execute_plan(plan_with_joints)
+
+    def go_to_pregrasp_position (object_pose, pregrasp_distance=0.3):
+        """ It should be already a box added to the scene"""
+        
+        if type (object_pose) is not geometry_msgs.msg.PoseStamped(): 
+            rospy.logerr ( "The object pose should be a geometry_msgs.msg.PoseStamped()")
+            return 
+
+        else:  
+            wpose = object_pose
+            wpose.pose.position.z += 0.3 #Go to 30 cm above the box 
+            plan_to_pregraps= self.ur10_commander.plan_to_pose_target (object_pose.pose.position.x, object_pose.pose.position.y, object_pose.pose.position.z, object_pose.pose.orientation.x, object_pose.pose.orientation.y, object_pose.pose.orientation.z, object_pose.pose.orientation.w)    
+
+            if len(plan_to_pos3.joint_trajectory.points)>0: 
+                print "Plan to pregrasp"
+                raw_input("if Plan is ok. press enter to execute")
+
+            else: 
+                rospy.logerr ( "Fail to make pregrasp plan")
+
+            ur10_commander.execute_plan(plan_to_pregraps)
+
+
+
+
+ 
+
+
+
+# # 1. Initialize
+# #=========================================================================================================
+
+if __name__ == '__main__':
+
+    rospy.init_node("move_commander_ur10", anonymous= True) # Define the node
+    position1= geometry_msgs.msg.PoseStamped() 
+    position1.pose.position.x = 0.92
+    position1.pose.position.y = 0.36
+    position1.pose.position.z = 0.25
+    
+    position2= geometry_msgs.msg.PoseStamped()
+    pick_and_place1 = PickAndPlace(position1, position2)
+
+
+
+# # 2. Go to initial position (Position 1)
+# # ========================================================================================================
+# #!!!!Change the values to pos 1 
+
+
+# # 3. Add box in position 2 
+# #=========================================================================================================
+
+# ur10_commander.add_box(0.92,0.36,0.25)
+    
+# time.sleep (5)  
+
+# # 4. Go to pregrasp position (30 cm above the box)
+# # ========================================================================================================
+# #NOT working 
+# def go_to_pregrasp_position (object_pose, pregrasp_distance=0.3):
+#     """ It should be already a box added to the scene"""
+        
+#     if type (object_pose) is not geometry_msgs.msg.PoseStamped(): 
+#         rospy.logerr ( "The object pose should be a geometry_msgs.msg.PoseStamped()")
+#         return 
+
+#     else:  
+#         wpose = object_pose
+#         wpose.position_z += 0.3 #Go to 30 cm above the box 
+#         ur10_commander.plan_to_pose_target (object_pose.position.x, object_pose.position.y, object_pose.position.z, object_pose.orientation.x, object_pose.orientation.y, object_pose.orientation.z, object_pose.orientation.w)    
+
+# plan_to_pregraps= go_to_pregrasp_position(ur10_commander, self.box_pose, pregrasp_distance=0.3)
+
+# if len(plan_to_pos3.joint_trajectory.points)>0: 
+#     print "Plan to 1"
+#     raw_input("if Plan is ok. press enter to execute")
+
+# else: 
+#     rospy.logerr ( "Fail to make a plan")
+
+# ur10_commander.execute_plan(plan_to_pregraps)
+
+# # 5. Go to grasp position (1 cm above the box)
+# # ========================================================================================================
+#    # THIS FUNCTION SHOULD BE ALSO IN PICK AND PLACE WITH THE SAME FORMAT THAT THE ONE BEFORE...
+#     # def go_to_grasp_position (self, grasp_distance):
+#     #     """ It should be already a box added to the scene"""
+        
+#     #     if self.box_name == "": 
+#     #         rospy.logerr ( "There is no box in the scene ")
+#     #         return 
+  
+#     #     else:  
+#     #         waypoints = []
+            
+#     #         # wpose = self.box_pose
+#     #         # wpose.position_z += 0.3 #Go to 30 cm above the box 
+#     #         # waypoints.append(copy.deepcopy(wpose))
+
+#     #         wpose = copy.deepcopy(self.box_pose)
+#     #         wpose.position_z += 0.01 #Go to 1 cm above the box 
+#     #         waypoints.append(copy.deepcopy(wpose))
+
+#     #         (plan, fraction) = self.group.compute_cartesian_path (waypoints,0.01,0.0)
+            
+#     #         return plan, fraction 
+
+
+# # 6. Attach the box
+# #=========================================================================================================
+
+# ur10_commander.attach_box()
+
+# time.sleep (5) 
+
+# # 7. Move the box to position 3. 
+# # ========================================================================================================
+# #!!!Change the values to pos3
+
+# plan_to_pos3 = ur10_commander.plan_to_pose_target(-1.2768, 0.36727, 0.26319,-0.013261,0.70689,0.70708 ,0.012899)
+
+# if len(plan_to_pos3.joint_trajectory.points)>0: 
+#     print "Plan to 1"
+#     raw_input("if Plan is ok. press enter to execute")
+
+# else: 
+#     rospy.logerr ( "Fail to make a plan")
+
+# ur10_commander.execute_plan(plan_to_pos3)
+
+
+# # 8. Detach the box 
+# # ========================================================================================================
+
+# ur10_commander.detach_box()
+# time.sleep (5) 
+
+# # 9. Go to vertical position 
+# # ======================================================================================================== 
+# plan_to_k= ur10_commander.plan_to_pose_target(0.04016, 0.36714, 1.52268,0.00021975, 0.70679, 0.70742, -0.00028481)
+     
+# if len(plan_to_k.joint_trajectory.points)>0: 
+#     print "Plan to i"
+#     raw_input("if Plan is ok. press enter to execute")
+
+# else: 
+#     rospy.logerr ( "Fail to make a plan")
+
+# ur10_commander.execute_plan(plan_to_k)
+
+# # 10. Delete the box
+# # =========================================================================================================
+
+# ur10_commander.remove_box()
+   
